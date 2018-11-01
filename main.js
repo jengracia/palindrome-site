09c4d2da0eedfdb9d93202b34b650c87dc1bc0da
Judith Engracia <judith.engracia@gmail.com>
2018-10-31 20:58:26 -0600

    Display the result on the page rather than as an alert

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let Phrase = require("jengracia-palindrome");
 
 function palindromeTester() {
   let string = prompt("Please enter a string for palindrome testing:");
-
   let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
   } else {
-    alert(`"${phrase.content}" is not a palindrome!`);
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome!`;
   }
 }
 
